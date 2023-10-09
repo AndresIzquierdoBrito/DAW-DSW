b35e7f878aa96de2f94ad990caafdca922b88425
andrew <andres.izbri@gmail.com>
2023-10-09 09:54:32 +0100

    First ASP NET MVC project

---
+++ First MVC Test/First MVC Test/Controllers/MyController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace First_MVC_Test.Controllers
+{
+    public class MyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Greetings()
+        {
+            return "Hey there!";
+        }
+
+        public string Goodbye()
+        {
+            return "See you later";
+        }
+    }
+}
