92014bfbb8fc687ef19551a17f36c1c77b13a2ff
andrew <andres.izbri@gmail.com>
2023-10-10 09:51:03 +0100

    Controllers, views and passing data

--- First MVC Test/First MVC Test/Controllers/MyController.cs
+++ First MVC Test/First MVC Test/Controllers/MyController.cs
@@ -8,15 +8,41 @@ namespace First_MVC_Test.Controllers
         {
             return View();
         }
+        public IActionResult Goodbye()
+        {
+            return View();
+        }
+
+        public IActionResult Felicita(int id, string name, int age)
+        {
+            ViewData["ID"] = id;
+            ViewData["name"] = name;
+            ViewData["age"] = age;
+
+            return View();
+        }
+       
 
         public string Greetings()
         {
             return "Hey there!";
         }
 
-        public string Goodbye()
+        public IActionResult actionName()
         {
-            return "See you later";
+            ViewData["name"] = "Johnson";
+            ViewData["nameTimes"] = 3;
+            return View();
         }
+        //public string Goodbye(int id, string name, int age)
+        //{
+        //    return $"El usuario con ID: {id} y nombre {name} se despisde a sus {age} años. DEP";
+        //}
+
+        //public string actionName(int ID, int year)
+        //{
+        //    return "El usuario con ID " + ID + " es del año: " + year;
+        //}
+
     }
 }
