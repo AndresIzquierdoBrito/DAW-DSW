94fb75571268fa58f0f30cd60638cc54d2d8163f
andrew <andres.izbri@gmail.com>
2023-10-17 09:42:24 +0100

    Finished A2 MVC

---
+++ AUT02_02_IzquierdoAndres_ModernFamily/AUT02_02_IzquierdoAndres_ModernFamily/Models/Personaje.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace AUT02_02_IzquierdoAndres_ModernFamily.Models
+{
+    public class Personaje
+    {
+        public int Id { get; set; }
+
+        [DisplayName ("Nombre")]
+        public string Name { get; set; }
+
+        [DisplayName("Familia")]
+        public string Family { get; set; }
+
+        [DisplayName("Número de hijos")]
+        public int NChildren { get; set; }
+    }
+}
