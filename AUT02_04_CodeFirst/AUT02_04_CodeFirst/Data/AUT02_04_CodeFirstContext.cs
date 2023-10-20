aa1c7fd9aa1ba94a888e34ba7fd5bdba67f17164
andrew <andres.izbri@gmail.com>
2023-10-20 13:59:17 +0100

    Minimal changs to ACT1, first steps in EF

---
+++ AUT02_04_CodeFirst/AUT02_04_CodeFirst/Data/AUT02_04_CodeFirstContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AUT02_04_CodeFirst.Models;
+
+namespace AUT02_04_CodeFirst.Data
+{
+    public class AUT02_04_CodeFirstContext : DbContext
+    {
+        public AUT02_04_CodeFirstContext (DbContextOptions<AUT02_04_CodeFirstContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<AUT02_04_CodeFirst.Models.Recipe> Recipe { get; set; } = default!;
+    }
+}
---
+++ AUT02_04_CodeFirst/AUT02_04_CodeFirst/Models/Recipe.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AUT02_04_CodeFirst.Models
+{
+    public class Recipe
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [DisplayName("Nombre")]
+        [Required(ErrorMessage = "Obligatorio")]
+        [MaxLength(50, ErrorMessage = "El nombre no puede tener más de 50 carácteres.")]
+        [MinLength(3, ErrorMessage = "El nombre no puede tener menos de 3 carácteres.")]
+        public string Name { get; set; }
+
+        [DisplayName("Descripción")]
+        [Required(ErrorMessage = "Obligatorio")]
+        [MaxLength(50, ErrorMessage = "El nombre no puede tener más de 50 carácteres.")]
+        [MinLength(3, ErrorMessage = "El nombre no puede tener menos de 3 carácteres.")]
+        public string Description { get; set; }
+
+        [DisplayName("Cantidad de KCal")]
+        [Range(1, 10000, ErrorMessage = "La cantidad de KCal ha de estar entre 1 y 10000")]
+        [Required(ErrorMessage = "Obligatorio")]
+        public int KCalAmount { get; set; }
+
+        [DisplayName("Ingredientes")]
+        [Required(ErrorMessage = "Obligatorio")]
+        [MaxLength(50, ErrorMessage = "La lista de ingredientes no puede tener más de 1000 carácteres.")]
+        [MinLength(1, ErrorMessage = "El nombre no puede tener menos de 1 carácter.")]
+        public string Ingredients { get; set; }
+
+    }
+}
