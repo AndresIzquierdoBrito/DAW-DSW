67d0ec0be84e05ce73361cc33e7b4132c4eb60ff
andrew <andres.izbri@gmail.com>
2023-11-24 13:56:21 +0000

    Progress in CodeFirstAPI

--- UT03_Ej02_AndresIzquierdo/UT03_Ej02_AndresIzquierdo/Controllers/GamesController.cs
+++ UT03_Ej02_AndresIzquierdo/UT03_Ej02_AndresIzquierdo/Controllers/GamesController.cs
@@ -25,33 +25,55 @@ namespace UT03_Ej02_AndresIzquierdo.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Game>>> GetGame()
         {
-          if (_context.Game == null)
-          {
-              return NotFound();
-          }
-            return await _context.Game.ToListAsync();
+            if (_context.Game == null)
+            {
+                return NotFound();
+            }
+            var games = await _context.Game.Select(g => new
+            {
+                GameId = g.GameId,
+                Title = g.Title,
+                Genre = new
+                {
+                    GenreId = g.GenreId,
+                    Name = g.Genre.Name
+                }
+            }).ToListAsync();
+
+            return Ok(games);
+
         }
 
         // GET: api/Games/5
         [HttpGet("{id}")]
         public async Task<ActionResult<Game>> GetGame(int id)
         {
-          if (_context.Game == null)
-          {
-              return NotFound();
-          }
-            var game = await _context.Game.FindAsync(id);
-
+            if (_context.Game == null)
+            {
+                return NotFound();
+            }
+            var game = await _context.Game
+               .Where(g => g.GameId == id)
+               .Select(g => new
+               {
+                   GameId = g.GameId,
+                   Title = g.Title,
+                   Genre = new
+                   {
+                       GenreId = g.GenreId,
+                       Name = g.Genre.Name
+                   }
+               })
+               .FirstOrDefaultAsync();
             if (game == null)
             {
                 return NotFound();
             }
 
-            return game;
+            return Ok(game);
         }
 
         // PUT: api/Games/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
         public async Task<IActionResult> PutGame(int id, Game game)
         {
@@ -78,24 +100,23 @@ namespace UT03_Ej02_AndresIzquierdo.Controllers
                 }
             }
 
-            return NoContent();
+            return CreatedAtAction("Get", game);
         }
 
         // POST: api/Games
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
         public async Task<ActionResult<Game>> PostGame(Game game)
         {
-          if (_context.Game == null)
-          {
-              return Problem("Entity set 'UT03_Ej02_AndresIzquierdoContext.Game'  is null.");
-          }
+            if (_context.Game == null)
+            {
+                return Problem("Entity set 'UT03_Ej02_AndresIzquierdoContext.Game'  is null.");
+            }
             _context.Game.Add(game);
             await _context.SaveChangesAsync();
 
             return CreatedAtAction("GetGame", new { id = game.GameId }, game);
         }
-
+        //https://www.phind.com/search?cache=ch8ju5t9torvy6c7sa3fevmy
         // DELETE: api/Games/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteGame(int id)
--- UT03_Ej02_AndresIzquierdo/UT03_Ej02_AndresIzquierdo/Program.cs
+++ UT03_Ej02_AndresIzquierdo/UT03_Ej02_AndresIzquierdo/Program.cs
@@ -1,18 +1,19 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using System.Text.Json.Serialization;
 using UT03_Ej02_AndresIzquierdo.Data;
 //using UT03_Ej02_AndresIzquierdo.Data;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<UT03_Ej02_AndresIzquierdoContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("UT03_Ej02_AndresIzquierdoContext") ?? throw new InvalidOperationException("Connection string 'UT03_Ej02_AndresIzquierdoContext' not found.")));
-builder.Services.AddDbContext<UT03_Ej02_AndresIzquierdoContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("UT03_Ej02_AndresIzquierdoContext") ?? throw new InvalidOperationException("Connection string 'UT03_Ej02_AndresIzquierdoContext' not found.")));
-
 // Add services to the container.
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers().AddJsonOptions(x => 
+    x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 
 var app = builder.Build();
 
