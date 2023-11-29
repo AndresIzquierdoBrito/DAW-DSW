1b348b086270286032450f3ea2c484eda6c6348a
andrew <andres.izbri@gmail.com>
2023-11-29 13:55:44 +0000

    API Auth

---
+++ ASP NET Core/API/AUT03_04_JuegosAPIAuth/AUT03_02_AndresIzquierdo_JuegosAPI/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Claims;
+using System.Security.Principal;
+
+namespace AUT03_02_AndresIzquierdo_JuegosAPI.Models
+{
+    public class User
+    {
+        private static int globalUserID = 0;
+
+        public ClaimsIdentity Id { get; private set; }
+
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public User(string userName, string email)
+        {
+            this.UserName = userName;
+            this.Email = email;
+        }
+    }
+}
