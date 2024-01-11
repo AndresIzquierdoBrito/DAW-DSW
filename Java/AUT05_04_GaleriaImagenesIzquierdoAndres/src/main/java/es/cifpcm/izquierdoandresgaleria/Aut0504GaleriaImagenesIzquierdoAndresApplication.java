9bdbd96f8c0109be83b3e40297a4856c00c00a7e
Andres Izquierdo <andres.izbri@gmail.com>
2024-01-11 22:24:53 +0000

    Started gallery Spring project

---
+++ Java/AUT05_04_GaleriaImagenesIzquierdoAndres/src/main/java/es/cifpcm/izquierdoandresgaleria/Aut0504GaleriaImagenesIzquierdoAndresApplication.java
@@ -0,0 +1,13 @@
+package es.cifpcm.izquierdoandresgaleria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aut0504GaleriaImagenesIzquierdoAndresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aut0504GaleriaImagenesIzquierdoAndresApplication.class, args);
+    }
+
+}
